b8fcf16a454492994be813529e85b72c8da9449b
noxan <noxan@byteweaver.net>
2012-11-18 22:14:51 +0100

    add move code function to nibble

--- shared/nibble.py
+++ shared/nibble.py
@@ -1,5 +1,5 @@
 from shared.geometry import Point
-from shared.utils import movement_to_energy
+from shared.utils import movement_to_energy, code_to_movement
 
 
 class Nibble(object):
@@ -9,6 +9,11 @@ class Nibble(object):
     def __init__(self, position=Point(0,0), energy=30):
         self._energy = energy
         self._position = position
+
+    def move_code(self, code):
+        code = int(code)
+        movement = code_to_movement(code)
+        return self.move_vector(movement[0], movement[1])
 
     def move(self, delta_x, delta_y):
         delta_energy = movement_to_energy(delta_x, delta_y)
