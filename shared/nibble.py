eca86779a358eedd7cdc43aa06e083ece19bfcdc
noxan <noxan@byteweaver.net>
2012-11-18 20:54:48 +0100

    add constructor to nibble class

--- shared/nibble.py
+++ shared/nibble.py
@@ -4,3 +4,6 @@ class Nibble(object):
     _energy = 0
     _position = Point(0, 0)
 
+    def __init__(self, position=Point(0,0), energy=30):
+        self._energy = energy
+        self._position = position
