30ff415983880e08937a38f729b863aac067a411
noxan <noxan@byteweaver.net>
2012-11-18 21:55:45 +0100

    add test for movement to energy function

--- tests/shared/tests.py
+++ tests/shared/tests.py
@@ -2,6 +2,20 @@ from nose.tools import assert_equal
 
 from shared.geometry import Point
 from shared.nibble import Nibble
+from shared.utils import movement_to_energy
+
+
+class TestUtils(object):
+    def test_movement_to_energy(self):
+        assert_equal(movement_to_energy(0,0), 1)
+        assert_equal(movement_to_energy(0,1), 2)
+        assert_equal(movement_to_energy(0,2), 5)
+        assert_equal(movement_to_energy(1,0), 2)
+        assert_equal(movement_to_energy(1,1), 3)
+        assert_equal(movement_to_energy(1,2), 6)
+        assert_equal(movement_to_energy(2,0), 5)
+        assert_equal(movement_to_energy(2,1), 6)
+        assert_equal(movement_to_energy(2,2), 7)
 
 
 class TestPoint(object):
