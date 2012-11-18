10b3f52000a1fa67b485d33ab1fca2c71831aa50
noxan <noxan@byteweaver.net>
2012-11-18 21:11:20 +0100

    add simple init test for point

--- tests/shared/tests.py
+++ tests/shared/tests.py
@@ -1,7 +1,14 @@
 from nose.tools import assert_equal
 
-from shared.nibble import Nibble
 from shared.geometry import Point
+from shared.nibble import Nibble
+
+
+class TestPoint(object):
+    def test_init(self):
+        p = Point(2,3)
+        assert_equal(p._x, 2)
+        assert_equal(p._y, 3)
 
 
 class TestNibble(object):
