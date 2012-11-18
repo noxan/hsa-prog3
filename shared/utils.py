d2b838400a1fc9c5a5cfdec3ec8af9afb1854711
noxan <noxan@byteweaver.net>
2012-11-18 21:55:24 +0100

    implement movement to energy function in utils

---
+++ shared/utils.py
@@ -0,0 +1,10 @@
+def movement_vector_to_energy(vector):
+    return movement_to_energy(vector.get_x(), vector.get_y())
+
+
+def movement_to_energy(delta_x, delta_y):
+    return {
+        0: {0: 1, 1: 2, 2: 5},
+        1: {0: 2, 1: 3, 2: 6},
+        2: {0: 5, 1: 6, 2: 7},
+    }[delta_x][delta_y]
