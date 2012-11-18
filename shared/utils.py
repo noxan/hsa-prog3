cc3b3df8d756729896da473bc6ac3553d3c95a58
noxan <noxan@byteweaver.net>
2012-11-18 22:14:25 +0100

    implement code to movement function in utils

--- shared/utils.py
+++ shared/utils.py
@@ -8,3 +8,9 @@ def movement_to_energy(delta_x, delta_y):
         1: {0: 2, 1: 3, 2: 6},
         2: {0: 5, 1: 6, 2: 7},
     }[delta_x][delta_y]
+
+
+def code_to_movement(code):
+    mx = (code % 5) - 2
+    my = 2 - int(code / 5)
+    return (mx, my)
