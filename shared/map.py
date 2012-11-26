5ea58ec62ecf678155686acfba5f422c8781fe07
noxan <noxan@byteweaver.net>
2012-11-26 16:38:01 +0100

    implement basic map with height and widht parameters

---
+++ shared/map.py
@@ -0,0 +1,11 @@
+class Map(object):
+    def __init__(self, width, height):
+        self._width = width
+        self._height = height
+
+    def get_width(self):
+        return self._width
+
+    def get_height(self):
+        return self._height
+
