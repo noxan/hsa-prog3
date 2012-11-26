7f0dca668f0165b522e4cc8b16a81c1e345ab01b
noxan <noxan@byteweaver.net>
2012-11-26 16:58:23 +0100

    implement clear function in map and call it in constructor

--- shared/map.py
+++ shared/map.py
@@ -2,6 +2,14 @@ class Map(object):
     def __init__(self, width, height):
         self._width = width
         self._height = height
+        self.clear()
+
+    def clear(self):
+        self._map = []
+        for x in range(self._width):
+            self._map.append([])
+            for y in range(self._height):
+                self._map[x].append('.')
 
     def get_width(self):
         return self._width
