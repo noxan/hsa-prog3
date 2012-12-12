b346e26290bf13faef3105cf2af4b0313ec6b989
noxan <noxan@byteweaver.net>
2012-12-12 16:41:32 +0100

    extend simple game test

--- tests/manual/simple_game.py
+++ tests/manual/simple_game.py
@@ -5,17 +5,16 @@ from shared.world import World, WorldStringRenderer
 world = World(10, 10)
 wrender = WorldStringRenderer(world)
 
-wrender.render()
-print "-------------------------"
-
-n = Nibble('a', world)
-print n
-
+a = Nibble('a', world)
+b = Nibble('b', world)
+c = Nibble('c', world)
 
 wrender.render()
-print n
+print a, b, c
 
-n.move_code(18)
+a.move(-1,1)
+b.move_code(18)
+c.move(-1,-2)
 
 wrender.render()
-print n
+print a, b, c
