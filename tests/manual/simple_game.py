2b16e276523e092fa2b7e96f2778fd47565b29c0
noxan <noxan@byteweaver.net>
2012-12-12 15:38:57 +0100

    add some more rounds to simple game test

--- tests/manual/simple_game.py
+++ tests/manual/simple_game.py
@@ -6,6 +6,16 @@ world = World(10, 10)
 wrender = WorldStringRenderer(world)
 
 wrender.render()
+print "-------------------------"
 
 n = Nibble('a', world)
+print n
+
+
+wrender.render()
+print n
+
+n.move_code(18)
+
+wrender.render()
 print n
