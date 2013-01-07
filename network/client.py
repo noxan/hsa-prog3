aa2d5ae6dc887b5bd11625366934bc6868a97977
noxan <noxan@byteweaver.net>
2013-01-07 13:06:06 +0100

    add connected handlin to client class to track connection state

--- network/client.py
+++ network/client.py
@@ -6,7 +6,11 @@ class Client(object):
         super(Client, self).__init__()
         self.host = host
         self.port = port
+        self.connected = False
 
     def connect(self):
+        if self.connected:
+            raise Exception("Client is already connected")
         self._socket = socket.socket()
         self._socket.connect((self.host, self.port))
+        self.connected = True
