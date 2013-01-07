ba4c88acf791dc30a7602c0257e16bc1545f14bd
noxan <noxan@byteweaver.net>
2013-01-07 13:07:20 +0100

    implement async network reading in client

--- network/client.py
+++ network/client.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import select
 
 
 class Client(threading.Thread):
@@ -14,9 +15,16 @@ class Client(threading.Thread):
             raise Exception("Client is already connected")
         self._socket = socket.socket()
         self._socket.connect((self.host, self.port))
+        self._socket.setblocking(0)
         self.connected = True
         self.run()
 
     def run(self):
         while self.connected:
-            pass
+            ready = select.select([self._socket], [], [], 1)
+            if ready[0]:
+                msg = self._socket.recv(1024)
+                if msg:
+                    print msg
+                else:
+                    self.connected = False
