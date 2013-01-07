aad3f7f7832f60725f283a393f1b591188bbb4ac
noxan <noxan@byteweaver.net>
2013-01-07 18:42:12 +0100

    write ugly but working test for config manager

--- tests/shared/config_tests.py
+++ tests/shared/config_tests.py
@@ -7,9 +7,8 @@ class TestConfigManager(object):
     def setup(self):
         self.c = ConfigManager("config.txt")
 
-    def test_read(self):
-        assert_equal(self.c.getConfig("runden"), 100)
-
-    def test_write(self):
+    def test_read_write(self):
+        assert_equal(self.c.getConfig("runden"), '100')
         self.c.setConfig("runden", 25)
-        assert_equal(self.c.getConfig("runden"), 25)
+        assert_equal(self.c.getConfig("runden"), '25')
+        self.c.setConfig("runden", 100) #reset
